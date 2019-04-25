c752ca82d2dad3c8f97b5731cb5f7add6e1272b0
gichimux <gichimumwai1@gmail.com>
2019-04-25 08:46:58 +0300

    registered blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,8 @@ def create_app(config_name):
     #initialize flask extensions
     bootstrap.init_app(app)
 
+    #register the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
